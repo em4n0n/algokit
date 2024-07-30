b77975d096fa7fd19ef5d15e41de9d91dca845ca
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-07-29 21:34:00 -0700

    Update algorand algopy

--- Digital_Marketplace/projects/Digital_Marketplace/smart_contracts/digital_marketplace/contract.py
+++ Digital_Marketplace/projects/Digital_Marketplace/smart_contracts/digital_marketplace/contract.py
@@ -1,20 +1,26 @@
-from algopy import ARC4Contract, String
-from algopy.arc4 import abimethod
+from algopy import *
 
 
 class DigitalMarketplace(ARC4Contract):
     assetId: UInt64
     unitaryPrice = UInt64
     # create the application
-    @arc4. abimethod(allow_ations=["NoOp"], create="require")
+    @arc4.abimethod(allow_ations=["NoOp"], create="require")
     def createApplication(self, assetID, unitaryPrice: UInt64) -> None:
         self.assetId = assetID
         self.unitaryPrice = unitaryPrice
     
     # update the listing price
+    @arc4.abimethod
+    def setPrice(self, unitaryPrice: UInt64): -> None:
+        assert Txn.sender == Global.creator_address
+        self.unitaryPrice = unitaryPrice
+
 
     # opt in to the asset that will be sold
-
+    @arc4_abimethod
+    def optInToAsset(self, mbrPay: gtxn.PaymentTransaction) -> None:
+        
     # buy the asset
 
     # delete the application
